db4caa0957cdab7858994e871acc2dc642aaa756
lazarodu <lazarodu@gmail.com>
2025-12-15 13:21:41 -0300

    feat: cors

--- api/main.py
+++ api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.routes import auth_router, loan_router, user_router, vinyl_router
 
@@ -7,6 +8,21 @@ app = FastAPI(
     description="API que irá servir os dados para os empréstimos",
     version="0.0.1",
 )
+
+origins = [
+    "http://http://localhost:3000",  # Vite local
+    "https://frontweb20252.vercel.app",  # Produção
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # lista de origens confiáveis
+    allow_credentials=True,
+    allow_methods=["*"],  # ou especifique ["GET", "POST"]
+    allow_headers=["*"],
+)
+
 
 app.include_router(user_router, prefix="/api", tags=["users"])
 app.include_router(auth_router, prefix="/api", tags=["auth"])
